9ab1d37fb5d65d098cb20a6bc9715ec8c8a56f96
BrandonJoe <brandon.joe42@yahoo.com>
2019-05-13 16:42:59 -0700

    testing 9

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-module.exports =  new Sequelize(process.env.DATABASE_URL, {
+module.exports =  new Sequelize(process.env.HEROKU_POSTGRESQL_MAUVE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
