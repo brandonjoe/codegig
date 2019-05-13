1621e9aea1000eb4987194d1d7a9e4da7a285eb0
BrandonJoe <brandon.joe42@yahoo.com>
2019-05-13 15:59:34 -0700

    adding database connection

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 
 module.exports = new Sequelize('codegig', 'postgres', 'Liquidtlo7!', {
-    host: 'postgresql-triangular-23249',
-    dialect: 'postgres',
+  host: process.env.DATABASE_URL,
+  ssl: true,
+    // dialect: 'postgres',
   });
