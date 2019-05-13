68a0112fcc5d5160e9aff3de2b95d216a1d1f489
BrandonJoe <brandon.joe42@yahoo.com>
2019-05-13 16:10:18 -0700

    trying again 2

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ const Sequelize = require('sequelize');
 
 module.exports = new Sequelize('codegig', 'postgres', 'Liquidtlo7!', {
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: true
   });
