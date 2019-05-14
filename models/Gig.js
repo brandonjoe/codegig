4472088e7f9dd9c391b6b9195fe42098aad21cde
BrandonJoe <brandon.joe42@yahoo.com>
2019-05-14 15:20:58 -0700

    deploying

--- models/Gig.js
+++ models/Gig.js
@@ -22,11 +22,11 @@ const Gig = db.define('gig', {
 Gig.sync({ force: true }).then(() => {
   
   return Gig.create({
-    Title: 'John',
-    technologies: 'Hancock',
-    description: 'react',
-    budget: '1000',
-    contact_email: 'user2312@gmail.com'
+    Title: ' REACT Developer (Santa monica)',
+    technologies: 'React, webpack, html, css, sass, node.js, Express, mySQL',
+    description: 'The Front-End Developer will be part of a team that builds the next generation enterprise user interface applications leveraging the latest and evolving set of user interface and technologies. Designs, modifies, develops, writes and implements user interface software programming applications, including software infrastructure and development tools for this new platform. ',
+    budget: '5000',
+    contact_email: 'contact@consumergeneration.com'
      
   });
 });
